d9a12bbb4d54f19d884fa157adefc53e49711939
Mayank Agrawal <MayankAgrawal2005>
2026-04-29 23:23:57 +0530

    change in the server.js

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,7 @@ require('dotenv').config();
 const app = express();
 
 // app.use(cors());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(cors()); // Allow all origins for production
 app.use(express.json());
 
 const uri = process.env.MONGO;
@@ -35,6 +32,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
